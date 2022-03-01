af58db9e4bff2d2d6a0c6561e1c204ac6b9d6c22
Michele Devetta <michele.devetta@cnr.it>
2022-03-01 19:02:01 +0100

    Added requirements and move to PostgreSQL

--- UdyniManagement/settings.py
+++ UdyniManagement/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'UdyniManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'management',
+        'USER' : 'management',
+        'PASSWORD' : 'ighech5ucha7ka',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
