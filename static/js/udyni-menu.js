0f713f10dc7a4bab5e53a46c36025762b0fcc964
Michele Devetta <michele.devetta@cnr.it>
2022-09-15 13:39:51 +0200

    Move of admin static file. Some updates to custom files. Added images for custom error pages.

--- static/js/udyni-menu.js
+++ static/js/udyni-menu.js
@@ -4,15 +4,23 @@ $(document).ready(function() {
   var current_location = window.location.pathname;
 
   // Search for all
+  var active_location = "";
+  var active_object;
   $("#accordionSidebar a.collapse-item").each(function() {
     if($(this).attr('href') != "") {
       //console.log("L:"+current_location+", H:"+$(this).attr('href'));
       if(current_location.startsWith($(this).attr('href'))) {
         // Found location
-        $(this).addClass('active');
-        $(this).closest("div.collapse").addClass('show');
-        $(this).closest("li.nav-item").addClass('active');
+        if($(this).attr('href').length > active_location.length) {
+          active_location = $(this).attr('href');
+          active_object = $(this);
+        }
       }
     }
   });
+  if(active_location.length > 0) {
+    active_object.addClass('active');
+    active_object.closest("div.collapse").addClass('show');
+    active_object.closest("li.nav-item").addClass('active');
+  }
 });
