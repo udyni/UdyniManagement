0d799ebfa53aa99453c52c920c1a4194501b0e89
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-12 01:07:50 +0200

    Created urls for Sample

--- LabLogbook/urls.py
+++ LabLogbook/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, reverse_lazy
 from UdyniManagement.views import EmptyView
 
+from django.urls import path, reverse_lazy
 from . import views
+
 
 urlpatterns = [
     path('', EmptyView.as_view(), name='lablogbook_index'),
@@ -10,6 +11,11 @@ urlpatterns = [
     path('laboratories/add', views.LaboratoryCreate.as_view(), name='laboratory_add'),
     path('laboratories/<int:pk>/modify', views.LaboratoryUpdate.as_view(), name='laboratory_mod'),
     path('laboratories/<int:pk>/delete', views.LaboratoryDelete.as_view(), name='laboratory_del'),
+    
+    path('samples/', views.SampleList.as_view(), name='sample_view'),
+    path('samples/add', views.SampleCreate.as_view(), name='sample_add'),
+    path('samples/<int:pk>/modify', views.SampleUpdate.as_view(), name='sample_mod'),
+    path('samples/<int:pk>/delete', views.SampleDelete.as_view(), name='sample_del'),
 ]
 
 menu = {
@@ -20,7 +26,7 @@ menu = {
         {
             'name': 'Laboratories',
             'link': reverse_lazy('laboratory_view'),
-            'permissions': ['Laboratory.laboratory_view'],
+            'permissions': ['Laboratory.laboratory_view',],
         },
         # TODO
         # {
@@ -28,11 +34,11 @@ menu = {
         #     'link': reverse_lazy('experimentalstation_view'),
         #     'permissions': [],
         # },
-        # {
-        #     'name': 'Samples',
-        #     'link': reverse_lazy('sample_view'),
-        #     'permissions': [],
-        # },
+        {
+            'name': 'Samples',
+            'link': reverse_lazy('sample_view'),
+            'permissions': ['Sample.sample_view',],
+        },
     ],
     'permissions': [],
 }
