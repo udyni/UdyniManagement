bd3fcaed1ae64d908c399acdfd7e12b2d2cae963
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-14 22:55:03 +0200

    removed unnecessary ordering for experiments

--- LabLogbook/models.py
+++ LabLogbook/models.py
@@ -105,7 +105,6 @@ class Experiment(models.Model):
     status = models.CharField(max_length=10, choices=POSSIBLE_STATUSES)
 
     class Meta:
-        ordering = ['creation_time', 'experiment_id']
         default_permissions = ()
         permissions = [
             ('experiment_view', 'View list of experiments'),
