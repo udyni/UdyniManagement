389b819460065b86348e8a9a069e3a8f9ff5c358
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-14 02:31:08 +0200

    beginning to merge laboratories with experimental station website pages + updated urls for better clarity

--- LabLogbook/urls.py
+++ LabLogbook/urls.py
@@ -7,28 +7,35 @@ from django.urls import path, reverse_lazy
 
 urlpatterns = [
     path('', EmptyView.as_view(), name='lablogbook_index'),
-
-    path('laboratories/', views.LaboratoryList.as_view(), name='laboratory_view'),
-    path('laboratories/add', views.LaboratoryCreate.as_view(), name='laboratory_add'),
-    path('laboratories/<int:pk>/modify', views.LaboratoryUpdate.as_view(), name='laboratory_mod'),
-    path('laboratories/<int:pk>/delete', views.LaboratoryDelete.as_view(), name='laboratory_del'),
     
+    # Samples
     path('samples/', views.SampleList.as_view(), name='sample_view'),
     path('samples/add', views.SampleCreate.as_view(), name='sample_add'),
-    path('samples/<int:pk>/modify', views.SampleUpdate.as_view(), name='sample_mod'),
-    path('samples/<int:pk>/delete', views.SampleDelete.as_view(), name='sample_del'),
+    path('samples/<int:sample_id>/modify', views.SampleUpdate.as_view(), name='sample_mod'),
+    path('samples/<int:sample_id>/delete', views.SampleDelete.as_view(), name='sample_del'),
 
+    # Experimental Stations
     path('experimentalstations/', views.ExperimentalStationList.as_view(), name='experimentalstation_view'),
-    path('experimentalstations/add', views.ExperimentalStationCreate.as_view(), name='experimentalstation_add'),
-    path('experimentalstations/<int:pk>/modify', views.ExperimentalStationUpdate.as_view(), name='experimentalstation_mod'),
-    path('experimentalstations/<int:pk>/delete', views.ExperimentalStationDelete.as_view(), name='experimentalstation_del'),
+    path('experimentalstations/laboratories/add', views.LaboratoryCreate.as_view(), name='laboratory_add'),
+    path('experimentalstations/laboratories/<int:laboratory_id>/modify', views.LaboratoryUpdate.as_view(), name='laboratory_mod'),
+    path('experimentalstations/laboratories/<int:laboratory_id>/delete', views.LaboratoryUpdate.as_view(), name='laboratory_del'),    
+    path('experimentalstations/laboratories/<int:laboratory_id>/add', views.ExperimentalStationCreate.as_view(), name='experimentalstation_add'),
+    path('experimentalstations/<int:station_id>/modify', views.ExperimentalStationUpdate.as_view(), name='experimentalstation_mod'),
+    path('experimentalstations/<int:station_id>/delete', views.ExperimentalStationDelete.as_view(), name='experimentalstation_del'),
 
+    # Experiments for experimental station
+    # TODO
     # look at personnel reporting page for arrrow that goes down
     # give the possibility of adding a sample to an experiment
     path('experimentalstations/<int:station_id>/experiments/', views.ExperimentList.as_view(), name='experiment_view'),
     path('experimentalstations/<int:station_id>/experiments/add', views.ExperimentCreate.as_view(), name='experiment_add'),
-    path('experimentalstations/<int:station_id>/experiments/<int:pk>/modify', views.ExperimentUpdate.as_view(), name='experiment_mod'),
-    path('experimentalstations/<int:station_id>/experiments/<int:pk>/delete', views.ExperimentDelete.as_view(), name='experiment_del'),
+    path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/modify', views.ExperimentUpdate.as_view(), name='experiment_mod'),
+    path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/delete', views.ExperimentDelete.as_view(), name='experiment_del'),
+    path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/add_sample/', views.ExperimentSampleAdd.as_view(), name='experiment_sample_add'),
+    path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/remove_sample/<int:sample_id>', views.ExperimentSampleRemove.as_view(), name='experiment_sample_del'),
+
+    # Logbook for experiment TODO
+    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/logbook', views.LogbookList.as_view(), name='logbook_view'),
 ]
 
 menu = {
@@ -37,14 +44,9 @@ menu = {
     'icon': 'fa-solid fa-book',
     'subsections': [
         {
-            'name': 'Laboratories',
-            'link': reverse_lazy('laboratory_view'),
-            'permissions': ['Laboratory.laboratory_view',],
-        },
-        {
             'name': 'Experimental Stations',
             'link': reverse_lazy('experimentalstation_view'),
-            'permissions': [],
+            'permissions': ['Laboratory.laboratory_view', 'ExperimentalStation.experimentalstation_view'],
         },
         {
             'name': 'Samples',
