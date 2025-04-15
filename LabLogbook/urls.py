09a2a02f194445e68a2171ae6fb781c6a9d660e3
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-15 18:37:08 +0200

    removed unnecessary urls

--- LabLogbook/urls.py
+++ LabLogbook/urls.py
@@ -31,20 +31,7 @@ urlpatterns = [
     path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/add_sample', views.SampleForExperimentAdd.as_view(), name='experiment_sample_add'),
     # note that here pk is the key of the SampleForExperiment object, not of the sample object
     path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/remove_sample/<int:pk>', views.SampleForExperimentRemove.as_view(), name='experiment_sample_del'),
-
-    # look at personnel reporting page for arrrow that goes down
-    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/measurements/', views.MeasurementList.as_view(), name='experiment_measurement_view'),
-    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/measurements/add', views.MeasurementAdd.as_view(), name='experiment_measurement_add'),
-    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/measurements/<int:pk>/modify', views.MeasurementUpdate.as_view(), name='experiment_measurement_mod'),
-    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/measurements/<int:pk>/delete', views.MeasurementDelete.as_view(), name='experiment_measurement_del'),
     
-    
-    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/measurements/<int:measurement_id>/add', views.FileAdd.as_view(), name='measurement_file_add'),
-    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/measurements/<int:measurement_id>/add', views.FileUpdate.as_view(), name='measurement_file_mod'),
-    # path('experimentalstations/<int:station_id>/experiments/<int:experiment_id>/measurements/<int:measurement_id>/add', views.FileDelete.as_view(), name='measurement_file_del'),
-
-
-
     # Logbook for experiment TODO
     # path('labs_and_experimentalstations/experimentalstations/<int:station_id>/experiments/<int:experiment_id>/logbook', views.CommentList.as_view(), name='comment_view'),
     # path('labs_and_experimentalstations/experimentalstations/<int:station_id>/experiments/<int:experiment_id>/logbook/add', views.CommentCreate.as_view(), name='comment_add'),
