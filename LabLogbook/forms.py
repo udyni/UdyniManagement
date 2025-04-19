837b0ac1a4f25fbaea1de4a2b92243e130da54da
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-20 00:41:59 +0200

    simplified comment forms

--- LabLogbook/forms.py
+++ LabLogbook/forms.py
@@ -4,20 +4,9 @@ from .models import Comment, CommentContent
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['experiment', 'measurement', 'type', 'parent']
-        widgets = {
-            'experiment': forms.HiddenInput(),
-            'measurement': forms.HiddenInput(),
-            'parent': forms.HiddenInput(),
-        }
+        fields = ['type']
 
 class CommentContentForm(forms.ModelForm):
     class Meta:
         model = CommentContent
-        fields = ['comment', 'version', 'author', 'timestamp', 'text']
-        widgets = {
-            'comment': forms.HiddenInput(),
-            'version': forms.HiddenInput(),
-            'author': forms.HiddenInput(),
-            'timestamp': forms.HiddenInput(),
-        }
+        fields = ['text']
